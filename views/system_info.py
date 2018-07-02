acf1de589dc4140ca396fe82dde59486a5bc594e
John Marzulli <john.marzulli@hotmail.com>
2018-07-01 23:01:47 -0700

    Add colors to the system info page.

--- views/system_info.py
+++ views/system_info.py
@@ -9,6 +9,7 @@ testing.load_imports()
 
 from lib.display import *
 from lib.task_timer import TaskTimer
+import lib.colors as colors
 import lib.local_debug as local_debug
 import units
 import configuration
@@ -33,16 +34,16 @@ def get_ip_address(ifname="wlan0"):
     if not local_debug.is_debug():
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            return socket.inet_ntoa(fcntl.ioctl(
+            return (socket.inet_ntoa(fcntl.ioctl(
                 s.fileno(),
                 0x8915,  # SIOCGIFADDR
-                struct.pack('256s', ifname[:15])
+                struct.pack('256s', ifname[:15]), GREEN)
             )[20:24])
         except:
             pass
 
     host_name = socket.gethostname()
-    return socket.gethostbyname(host_name)
+    return (socket.gethostbyname(host_name), GREEN)
 
 
 def get_cpu_temp():
@@ -53,14 +54,23 @@ def get_cpu_temp():
         string -- The CPU temp to display
     """
 
+    color = GREEN
+
     try:
         raspberry_pi_temp = open('/sys/class/thermal/thermal_zone0/temp')
         temp = float(raspberry_pi_temp.read())
         temp = temp/1000
 
-        return "{0}C".format(temp)
+        if temp > 90:
+            color = RED
+        if temp > 50:
+            delta = 90 - temp
+            proportion = delta / 40.0
+            color = colors.get_color_mix(GREEN, RED, proportion)
+
+        return ("{0}C".format(temp), color)
     except:
-        return '---'
+        return ('---', GRAY)
 
 
 def get_websocket_uptime():
@@ -76,15 +86,15 @@ def get_websocket_uptime():
         ) - AdsbTrafficClient.INSTANCE.create_time).total_seconds()
 
         if connection_uptime < 60:
-            return "{} seconds".format(int(connection_uptime))
+            return ("{} seconds".format(int(connection_uptime)), YELLOW)
         elif connection_uptime < 360:
-            return "{0:.1f} minutes".format(connection_uptime / 60)
+            return ("{0:.1f} minutes".format(connection_uptime / 60), GREEN)
         elif connection_uptime < 216000:
-            return "{0:.1f} hours".format(connection_uptime / 3600)
+            return ("{0:.1f} hours".format(connection_uptime / 3600), GREEN)
         else:
-            return "{0:.1f} days".format(connection_uptime / 216000)
+            return ("{0:.1f} days".format(connection_uptime / 216000), GREEN)
     else:
-        return "DISCONNECTED"
+        return ("DISCONNECTED", RED)
 
 
 class SystemInfo(AhrsElement):
@@ -117,18 +127,24 @@ class SystemInfo(AhrsElement):
         # First line in the array is at the bottom.
         # Last line in the array is towards the top.
         info_lines = [
-            "IP         : {}".format(get_ip_address()),
-            "CPU        : {}".format(get_cpu_temp()),
-            "SOCKET     : {}".format(get_websocket_uptime()),
-            "DECLINATION: {}".format(configuration.CONFIGURATION.get_declination()),
-            "OWNSHIP    : {}".format(configuration.CONFIGURATION.ownship)
+            ["IP         : ", get_ip_address()],
+            ["CPU        : ", get_cpu_temp()],
+            ["SOCKET     : ", get_websocket_uptime()],
+            ["DECLINATION: ", [str(configuration.CONFIGURATION.get_declination()), BLUE]],
+            ["OWNSHIP    : ", [configuration.CONFIGURATION.ownship, BLUE]]
         ]
 
         render_y = self.__text_y_pos__
 
         for line in info_lines:
-            texture = self.__font__.render(line, True, BLUE, BLACK)
-            framebuffer.blit(texture, (0, render_y))
+            # Draw the label in a standard color.
+            texture_lhs = self.__font__.render(line[0], True, BLUE, BLACK)
+            framebuffer.blit(texture_lhs, (0, render_y))
+            size = texture_lhs.get_size()
+
+            # Draw the value in the encoded colors.
+            texture_rhs = self.__font__.render(line[1][0], True, line[1][1], BLACK)
+            framebuffer.blit(texture_rhs, (size[0], render_y))
 
             render_y = render_y - (self.font_height * 1.2)
 
