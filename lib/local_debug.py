cd618dbbfb77f288fdc019a439809bd302173eda
John Marzulli <john.marzulli@hotmail.com>
2018-08-13 22:26:47 -0700

    Enforce the use of the correct Python version.

--- lib/local_debug.py
+++ lib/local_debug.py
@@ -6,6 +6,32 @@ debugging on a Mac or Windows host.
 
 from sys import platform
 
+from sys import platform, version_info
+
+REQUIRED_PYTHON_VERSION = 2.7
+MAXIMUM_PYTHON_VERSION = 2.7
+
+
+def validate_python_version():
+    """
+    Checks to make sure that the correct version of Python is being used.
+
+    Raises:
+        Exception -- If the  version of Python is not new enough.
+    """
+
+    python_version = float('{}.{}'.format(
+        version_info.major, version_info.minor))
+    error_text = 'Requires Python {}'.format(REQUIRED_PYTHON_VERSION)
+
+    if python_version < REQUIRED_PYTHON_VERSION:
+        print(error_text)
+        raise Exception(error_text)
+
+    if python_version > MAXIMUM_PYTHON_VERSION:
+        print('Python version {} is newer than the maximum allowed version of {}'.format(
+            python_version, MAXIMUM_PYTHON_VERSION))
+
 
 def is_debug():
     """
@@ -13,3 +39,5 @@ def is_debug():
     """
 
     return platform in ["win32", "darwin"]
+
+validate_python_version()
