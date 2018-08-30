88082145ddef00c91f92191ce2daefe96f9ada6e
John Marzulli <john.marzulli@hotmail.com>
2018-08-29 22:05:46 -0700

    Fix issue with the REST socket being unable to be opened.

--- restful_host.py
+++ restful_host.py
@@ -208,13 +208,7 @@ class HudServer(object):
             string -- The IP address of this server.
         """
 
-        local_ip = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(
-            ('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
-
-        if local_debug.is_debug():
-            local_ip = ''
-
-        return local_ip
+        return ''
 
     def run(self):
         """
