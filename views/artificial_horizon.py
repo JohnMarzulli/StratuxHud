891e586c459fad31701093ae65cdc6bf4b498e5e
John Marzulli <john.marzulli@hotmail.com>
2018-10-08 19:58:03 -0700

    Use the black background without Alpha for the AH ladder again.

--- views/artificial_horizon.py
+++ views/artificial_horizon.py
@@ -22,9 +22,9 @@ class ArtificialHorizon(AhrsElement):
 
         for reference_angle in range(-degrees_of_pitch, degrees_of_pitch + 1, 10):
             text = font.render(str(reference_angle),
-                               True,
-                               WHITE)
-                               # BLACK).convert()
+                               False,
+                               WHITE,
+                               BLACK).convert()
             size_x, size_y = text.get_size()
             self.__pitch_elements__[reference_angle] = (
                 text, (size_x >> 1, size_y >> 1))
