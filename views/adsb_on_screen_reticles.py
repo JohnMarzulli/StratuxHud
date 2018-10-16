94643522561cff105113446807104c05ec17913d
John Marzulli <john.marzulli@hotmail.com>
2018-10-15 20:50:27 -0700

    Render reticles faster.

--- views/adsb_on_screen_reticles.py
+++ views/adsb_on_screen_reticles.py
@@ -27,51 +27,66 @@ class AdsbOnScreenReticles(AdsbElement):
         self.__top_border__ = int(self.__height__ * 0.1)
         self.__bottom_border__ = self.__height__ - self.__top_border__
 
+    def __render_on_screen_reticle__(self,  framebuffer, orientation, traffic):
+        """
+        Draws a single reticle on the screen.
+
+        Arguments:
+            framebuffer {Surface} -- Render target
+            orientation {Orientation} -- The orientation of the plane.
+            traffic {Traffic} -- The traffic to draw the reticle for.
+        """
+
+        identifier = traffic.get_identifer()
+
+        # Find where to draw the reticle....
+        reticle_x, reticle_y = self.__get_traffic_projection__(
+            orientation, traffic)
+
+        # Render using the Above us bug
+        on_screen_reticle_scale = get_reticle_size(traffic.distance)
+        reticle, reticle_size_px = self.get_onscreen_reticle(
+            reticle_x, reticle_y, on_screen_reticle_scale)
+
+        reticle_x, reticle_y = self.__rotate_reticle__([[reticle_x, reticle_y]],
+                                                       orientation.roll)[0]
+
+        self.__render_target_reticle__(framebuffer,
+                                       identifier,
+                                       (reticle_x, reticle_y),
+                                       reticle,
+                                       orientation.roll,
+                                       reticle_size_px)
+
     def render(self, framebuffer, orientation):
+        """
+        Renders all of the on-screen reticles  for nearby traffic.
+
+        Arguments:
+            framebuffer {Surface} -- The render target.
+            orientation {Orientation} -- The orientation of the plane the HUD is in.
+        """
+
         self.task_timer.start()
         # Get the traffic, and bail out of we have none
         traffic_reports = HudDataCache.get_reliable_traffic()
 
-        if traffic_reports is None:
-            self.task_timer.stop()
-            return
+        # Do not render reticles for things to far away
+        traffic_reports = filter(lambda x: x.distance < imperial_occlude and not x.is_on_ground(),
+                                 traffic_reports)
 
-        for traffic in traffic_reports:
-            # Do not render reticles for things to far away
-            if traffic.distance > imperial_occlude:
-                continue
+        [self.__render_on_screen_reticle__(framebuffer, orientation, traffic)
+         for traffic in traffic_reports]
 
-            if traffic.is_on_ground():
-                continue
-
-            identifier = traffic.get_identifer()
-
-            # Find where to draw the reticle....
-            reticle_x, reticle_y = self.__get_traffic_projection__(
-                orientation, traffic)
-
-            # Render using the Above us bug
-            on_screen_reticle_scale = get_reticle_size(traffic.distance)
-            reticle, reticle_size_px = self.get_onscreen_reticle(
-                reticle_x, reticle_y, on_screen_reticle_scale)
-
-            if reticle_y < self.__top_border__ or reticle_y > self.__bottom_border__ or \
-                    reticle_x < 0 or reticle_x > self.__width__:
-                continue
-            else:
-                reticle_x, reticle_y = self.__rotate_reticle__(
-                    [[reticle_x, reticle_y]], orientation.roll)[0]
-
-                self.__render_target_reticle__(
-                    framebuffer, identifier, reticle_x, reticle_y, reticle, orientation.roll, reticle_size_px)
         self.task_timer.stop()
 
-    def __render_target_reticle__(self, framebuffer, identifier, center_x, center_y, reticle_lines, roll, reticle_size_px):
+    def __render_target_reticle__(self, framebuffer, identifier, pos, reticle_lines, roll, reticle_size_px):
         """
         Renders a targetting reticle on the screen.
         Assumes the X/Y projection has already been performed.
         """
 
+        center_x, center_y = pos
         border_space = int(reticle_size_px * 1.2)
 
         center_y = border_space if center_y < border_space else center_y
