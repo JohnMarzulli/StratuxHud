772df65e5c963564b3dd4df3d2a593eaf2650ac1
John Marzulli <jmarzulli@pop-os.localdomain>
2019-05-19 22:14:47 -0700

    Try to kill the processes so that the parent is last.

--- kill_huds.sh
+++ kill_huds.sh
@@ -3,4 +3,4 @@
 # Kills any StratuxHud processes hanging arround.
 # Useful for making sure the REST ports are clear
 # and nothing is left over.
-sudo kill -9 $(ps -ef | grep -i "hud" | grep -v "grep" | awk '{print $2}')
+sudo kill -9 $(ps -ef | grep -i "hud" | grep -v "grep" |  awk '{print $2}' | tac)
