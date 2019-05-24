2fd86876d681cc785501654000a47413ce6a6a93
John Marzulli <jmarzulli@pop-os.localdomain>
2019-05-24 11:13:37 -0700

    Make sure all instaces are killed.

--- kill_huds.sh
+++ kill_huds.sh
@@ -3,4 +3,4 @@
 # Kills any StratuxHud processes hanging arround.
 # Useful for making sure the REST ports are clear
 # and nothing is left over.
-sudo kill -9 $(ps -ef | grep -i "hud" | grep -v "grep" |  awk '{print $2}' | tac)
+sudo kill -9 $(sudo ps -ef | grep -i "hud" | grep -v "grep" |  awk '{print $2}' | tac)
