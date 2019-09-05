b959729f23b1c68d0f8ba44d76e4d6f806db3a9f
john.marzulli@hotmail.com <jmarzulli@pop-os.localdomain>
2019-09-04 22:34:51 -0700

    Splash screen installer for modifying base Stratux image.

---
+++ install_splash.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+sudo apt-get install fbi
+sudo cp media/hud_logo.jpg /etc/splash.jpg
+
+FILE = /etc/init.d/asplashscreen
+
+if [ -f "$FILE" ]; then
+    echo "Skipping"
+else
+    echo "Setting up boot logo."
+
+    cp asplashscreen /etc/init.d/
+    sudo chmod a+x /etc/init.d/asplashscreen
+    sudo insserv /etc/init.d/asplashscreen
+fi
