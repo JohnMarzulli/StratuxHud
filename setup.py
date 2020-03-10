ac0fc1119b3b333adf7a52086d6f92ab57e3878c
John Marzulli <john.marzulli@hotmail.com>
2020-03-10 02:26:44 -0700

    Fix dependency installs.

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-import lib.local_debug as local_debug
-import configuration
 from setuptools import setup
 
 installs = ['pytest',
@@ -9,13 +7,13 @@ installs = ['pytest',
             'bluepy']
 
 
-setup(name='StratuxHud',
-      version=configuration.VERSION,
-      python_requires='>=2.7',
-      description='Graphics for a Heads Up Display projector powered by a Stratux receiver.',
-      url='https://github.com/JohnMarzulli/StratuxHud',
-      author='John Marzulli',
-      author_email='john.marzulli@hotmail.com',
-      license='GPL V3',
-      install_requires=installs
-      )
+setup(
+    name='StratuxHud',
+    version='1.7.0',
+    python_requires='>=2.7',
+    description='Graphics for a Heads Up Display projector powered by a Stratux receiver.',
+    url='https://github.com/JohnMarzulli/StratuxHud',
+    author='John Marzulli',
+    author_email='john.marzulli@hotmail.com',
+    license='GPL V3',
+    install_requires=installs)
