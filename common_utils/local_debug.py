66315bca546b9ffbc76b9945e038d93618155a8c
John Marzulli <john.marzulli@hotmail.com>
2020-04-29 04:18:39 -0700

    Allow python 3.8

--- common_utils/local_debug.py
+++ common_utils/local_debug.py
@@ -9,7 +9,7 @@ from sys import platform as os_platform
 from sys import version_info
 
 REQUIRED_PYTHON_VERSION = 3.5
-MAXIMUM_PYTHON_VERSION = 3.7
+MAXIMUM_PYTHON_VERSION = 3.8
 
 IS_LINUX = 'linux' in os_platform
 DETECTED_CPU = platform.machine()
