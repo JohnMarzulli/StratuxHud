fa882a0984db8a8e32f60b1fec8be86a2612650e
John Marzulli <john.marzulli@hotmail.com>
2020-11-27 16:43:07 -0800

    Make sure that the stable version of PyGame is used.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from common_utils.local_debug import IS_PI
 
 installs = ['pytest',
-            'pygame',
+            'pygame==1.9.6',
             'requests']
 
 if IS_PI:
@@ -11,7 +11,7 @@ if IS_PI:
 
 setup(
     name='StratuxHud',
-    version='1.8.0',
+    version='2.0',
     python_requires='>=3.8',
     description='Graphics for a Heads Up Display projector powered by a Stratux receiver.',
     url='https://github.com/JohnMarzulli/StratuxHud',
