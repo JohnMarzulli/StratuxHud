9be436abb25950c8fb16ee5adf8c8cd063ab5cb1
John Marzulli <john.marzulli@hotmail.com>
2020-12-21 20:58:03 -0800

    Add multple rounds to the benchmark to add more power and consistency.

--- hud_benchmark.py
+++ hud_benchmark.py
@@ -24,56 +24,69 @@ random_angles = [random.randrange(
 random_radians = [math.radians(random.randrange(
     0, 360) + ((random.randrange(0, 9) / 10))) for i in range(1, max_test_calls)]
 
-# Test trig functions
-with TaskProfiler("math::trig_calced"):
-    for rand_val in random_angles:
-        radians = math.radians(rand_val)
-        cos_rand = math.cos(radians)
-        sin_rant = math.sin(radians)
+for attempt in range(0, 4):
+    LOGGER.log_info_message("Round #{}".format(attempt))
 
-with TaskProfiler("math::trig_cached"):
-    for rand_val in random_angles:
-        int_degs = int(rand_val)
-        cos_rand = fast_math. COS_BY_DEGREES[int_degs]
-        sin_rant = fast_math.SIN_BY_DEGREES[int_degs]
+    LOGGER.log_info_message("    trig::calced")
+    # Test trig functions
+    with TaskProfiler("trig::calced"):
+        for rand_val in random_angles:
+            radians = math.radians(rand_val)
+            cos_rand = math.cos(radians)
+            sin_rant = math.sin(radians)
 
-with TaskProfiler("math::degrees_to_radians_calced"):
-    for rand_val in random_angles:
-        radians = math.radians(rand_val)
+    LOGGER.log_info_message("    trig::cached")
+    with TaskProfiler("trig::cached"):
+        for rand_val in random_angles:
+            int_degs = int(rand_val)
+            cos_rand = fast_math. COS_BY_DEGREES[int_degs]
+            sin_rant = fast_math.SIN_BY_DEGREES[int_degs]
 
-with TaskProfiler("math::degrees_to_radians_cached"):
-    for rand_val in random_angles:
-        radians = fast_math.get_radians(rand_val)
+    LOGGER.log_info_message("    degrees_to_radians::calced")
+    with TaskProfiler("degrees_to_radians::calced"):
+        for rand_val in random_angles:
+            radians = math.radians(rand_val)
 
-with TaskProfiler("math::radians_to_degrees_calced"):
-    for rand_val in random_radians:
-        degrees = math.degrees(rand_val)
+    LOGGER.log_info_message("    degrees_to_radians::cached")
+    with TaskProfiler("degrees_to_radians::cached"):
+        for rand_val in random_angles:
+            radians = fast_math.get_radians(rand_val)
 
-with TaskProfiler("math::radians_to_degrees_cached"):
-    for rand_val in random_angles:
-        degrees = fast_math.get_degrees(rand_val)
+    LOGGER.log_info_message("    radians_to_degrees::calced")
+    with TaskProfiler("radians_to_degrees::calced"):
+        for rand_val in random_radians:
+            degrees = math.degrees(rand_val)
 
-with TaskProfiler("math::mult_times"):
-    for rand_val in random_angles:
-        radians = rand_val * 2.0
-        radians = rand_val * 4.0
+    LOGGER.log_info_message("    radians_to_degrees::cached")
+    with TaskProfiler("radians_to_degrees::cached"):
+        for rand_val in random_angles:
+            degrees = fast_math.get_degrees(rand_val)
 
-with TaskProfiler("math::mult_shifted"):
-    for rand_val in random_angles:
-        as_int = int(rand_val)
-        radians = as_int << 1
-        radians = as_int << 2
+    LOGGER.log_info_message("    mult::operator")
+    with TaskProfiler("mult::operator"):
+        for rand_val in random_angles:
+            radians = rand_val * 2.0
+            radians = rand_val * 4.0
 
-with TaskProfiler("math::div_divved"):
-    for rand_val in random_angles:
-        radians = rand_val / 2.0
-        radians = rand_val / 4.0
+    LOGGER.log_info_message("    mult::shifted")
+    with TaskProfiler("mult::shifted"):
+        for rand_val in random_angles:
+            as_int = int(rand_val)
+            radians = as_int << 1
+            radians = as_int << 2
 
-with TaskProfiler("math::div_shifted"):
-    for rand_val in random_angles:
-        as_int = int(rand_val)
-        radians = as_int >> 1
-        radians = as_int >> 2
+    LOGGER.log_info_message("    div::operator")
+    with TaskProfiler("div::operator"):
+        for rand_val in random_angles:
+            radians = rand_val / 2.0
+            radians = rand_val / 4.0
+
+    LOGGER.log_info_message("    div::shifted")
+    with TaskProfiler("div::shifted"):
+        for rand_val in random_angles:
+            as_int = int(rand_val)
+            radians = as_int >> 1
+            radians = as_int >> 2
 
 TaskProfiler.log(LOGGER)
 
