7b0f3e2d4f27a5d5c413375b90105fd62638431b
John Marzulli <john.marzulli@hotmail.com>
2021-02-02 08:47:08 -0800

    Add docs to the math functions.

--- common_utils/fast_math.py
+++ common_utils/fast_math.py
@@ -69,6 +69,15 @@ def wrap_radians(
 def get_radians(
     degrees: float
 ) -> float:
+    """
+    Given an angle in degrees, returns the angle in radians.
+
+    Args:
+        degrees (float): The angle in degrees.
+
+    Returns:
+        float: The angle in radians.
+    """
     clamped_degrees = int(wrap_degrees(degrees))
     return __RADIANS_BY_DEGREES__[clamped_degrees]
 
@@ -76,6 +85,15 @@ def get_radians(
 def get_degrees(
     radians: float
 ) -> float:
+    """
+    Given an angle in radians, returns the angle in degrees.
+
+    Args:
+        radians (float): The angle to convert, in radians.
+
+    Returns:
+        float: The angle converted to degrees.
+    """
     clamped_radians = wrap_radians(radians)
     indexed_radians = int(clamped_radians * 100)
 
@@ -85,6 +103,15 @@ def get_degrees(
 def sin(
     degrees: float
 ) -> float:
+    """
+    Get the sin of an angle in degrees.
+
+    Args:
+        degrees (float): The angle in degrees.
+
+    Returns:
+        float: The sin of the angle.
+    """
     clamped_degs = int(wrap_degrees(degrees))
     return SIN_BY_DEGREES[clamped_degs]
 
@@ -92,6 +119,15 @@ def sin(
 def cos(
     degrees: float
 ) -> float:
+    """
+    Get the cos of an angle in degrees.
+
+    Args:
+        degrees (float): The angle in degrees.
+
+    Returns:
+        float: The cos of the angle.
+    """
     clamped_degs = int(wrap_degrees(degrees))
     return COS_BY_DEGREES[clamped_degs]
 
@@ -99,6 +135,15 @@ def cos(
 def tan(
     degrees: float
 ) -> float:
+    """
+    Get the tan of an angle in degrees.
+
+    Args:
+        degrees (float): The angle in degrees.
+
+    Returns:
+        float: The tan of the angle.
+    """
     clamped_degs = int(wrap_degrees(degrees))
     return TAN_BY_DEGREES[clamped_degs]
 
@@ -107,8 +152,19 @@ def translate_points(
     list_of_points: list,
     translation: list
 ) -> list:
+    """
+    Moves a set of points by the given x/y component.
+
+    Args:
+        list_of_points (list): The points to translate.
+        translation (list): How much to translate the points by.
+
+    Returns:
+        list: The original points that have been translated.
+    """
+
     return [[point[0] + translation[0],
-            point[1] + translation[1]] for point in list_of_points]
+             point[1] + translation[1]] for point in list_of_points]
 
 
 def rotate_points(
@@ -116,13 +172,16 @@ def rotate_points(
     rotation_center: list,
     rotation_degrees: float
 ) -> list:
-    """Generates the coordinates for a reticle indicating
-    traffic is above use.
+    """
+    Rotates all of the given points around the given center.
 
-    Arguments:
-        center_x {int} -- Center X screen position
-        center_y {int} -- Center Y screen position
-        scale {float} -- The scale of the reticle relative to the screen.
+    Args:
+        list_of_points (list): The points to rotate.
+        rotation_center (list): The point of rotation. This is so the point of rotation can be other than 0,0
+        rotation_degrees (float): How much to rotate the points by.
+
+    Returns:
+        list: The collection of points that have been rotated.
     """
 
     # 2 - determine the angle of rotation compared to our "up"
