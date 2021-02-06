265084409a02dd82f8a87e5d0dbd7b0e5b7f511f
John Marzulli <john.marzulli@hotmail.com>
2021-02-06 03:19:46 -0800

    Documentation and cleanup

--- views/altitude.py
+++ views/altitude.py
@@ -1,3 +1,7 @@
+"""
+Element to render the Altitude.
+"""
+
 from numbers import Number
 
 from data_sources.ahrs_data import AhrsData
@@ -6,7 +10,54 @@ from rendering import colors
 from views.ahrs_element import AhrsElement
 
 
+def __get_indicated_text__(
+    orientation: AhrsData,
+    color: list
+) -> list:
+    """
+    Get the text package (scale, text, color) for the given altitude.
+
+    Args:
+        orientation (AhrsData): [description]
+        color (list): [description]
+
+    Returns:
+        list: A list of text packages that allow for the altitude
+        and units to be drawn using vertical stacking.
+    """
+    is_altitude_valid = orientation.alt is not None and isinstance(
+        orientation.alt,
+        Number)
+
+    text_package = []
+    alt_value = str(int(orientation.alt)) \
+        if is_altitude_valid else AhrsElement.INOPERATIVE_TEXT
+    color = colors.WHITE if is_altitude_valid else colors.RED
+
+    text_package.append(alt_value)
+    text_package.append("ft " if is_altitude_valid else "   ")
+    text_package.append("MSL")
+
+    is_first = True
+    text_with_scale_and_color = []
+
+    for text_piece in text_package:
+        scale = 1.0 if is_first else 0.5
+        package = [scale, text_piece, color]
+
+        text_with_scale_and_color.append(package)
+
+        is_first = False
+
+    return text_with_scale_and_color
+
+
 class Altitude(AhrsElement):
+    """
+    Element to render the Altitude.
+    """
+
+    # pylint:disable=unused-argument
     def __init__(
         self,
         degrees_of_pitch: float,
@@ -20,47 +71,23 @@ class Altitude(AhrsElement):
 
         self.__text_y_pos__ = alt_y_pos - self.__font_half_height__
 
-    def __get_indicated_text__(
-        self,
-        orientation: AhrsData,
-        color: list
-    ) -> list:
-        is_altitude_valid = orientation.alt is not None and isinstance(
-            orientation.alt,
-            Number)
-
-        text_package = []
-        alt_value = str(int(orientation.alt)) \
-            if is_altitude_valid else AhrsElement.INOPERATIVE_TEXT
-        color = colors.WHITE if is_altitude_valid else colors.RED
-
-        text_package.append(alt_value)
-        text_package.append("ft " if is_altitude_valid else "   ")
-        text_package.append("MSL")
-
-        is_first = True
-        text_with_scale_and_color = []
-
-        for text_piece in text_package:
-            scale = 1.0 if is_first else 0.5
-            package = [scale, text_piece, color]
-
-            text_with_scale_and_color.append(package)
-
-            is_first = False
-
-        return text_with_scale_and_color
-
     def render(
         self,
         framebuffer,
         orientation: AhrsData
     ):
+        """
+        Render the altitude to the framebuffer
+
+        Args:
+            framebuffer: The framebuffer/texture to render to.
+            orientation (AhrsData): The current AHRS information for the aircraft.
+        """
         is_altitude_valid = orientation.alt is not None and isinstance(
             orientation.alt,
             Number)
         color = colors.WHITE if is_altitude_valid else colors.RED
-        annotated_text = self.__get_indicated_text__(orientation, color)
+        annotated_text = __get_indicated_text__(orientation, color)
         self.__render_text_with_stacked_annotations_right_justified__(
             framebuffer,
             [self.__right_border__, self.__text_y_pos__],
