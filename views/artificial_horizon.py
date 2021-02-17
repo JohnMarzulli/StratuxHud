78e5085c2e8f512949554df475ddf172f113f67c
John Marzulli <john.marzulli@hotmail.com>
2021-02-17 04:51:24 -0800

    Improve culling, draw horizon as two segments

--- views/artificial_horizon.py
+++ views/artificial_horizon.py
@@ -24,20 +24,20 @@ class ArtificialHorizon(AhrsElement):
     ):
         super().__init__(font, framebuffer_size)
 
-        self.__long_line_width__ = self.__framebuffer_size__[0] * 0.4
-        self.__short_line_width__ = self.__framebuffer_size__[0] * 0.2
+        self.__long_segment_length__ = int(self.__framebuffer_size__[0] * 0.4)
+        self.__short_segment_length__ = int(self.__framebuffer_size__[0] * 0.2)
+        self.__inner_blank_area_length__ = self.__short_segment_length__ / 2
         self.__pixels_per_degree_y__ = int(pixels_per_degree_y)
-        self.__height__ = framebuffer_size[1]
 
         self.__reference_angles__ = range(
             -degrees_of_pitch,
             degrees_of_pitch + 1,
             10)
 
-    def __render_reference_line__(
+    def __render_horizon_reference__(
         self,
         framebuffer,
-        line_info,
+        segments_info,
         roll: float
     ):
         """
@@ -50,15 +50,24 @@ class ArtificialHorizon(AhrsElement):
             roll {float} -- How much the plane is rolled.
         """
 
-        line_coords, line_center, reference_angle = line_info
-        drawing.segments(
-            framebuffer,
-            colors.GREEN,
-            False,
-            line_coords,
-            self.__line_width__)
+        segments, line_center, reference_angle = segments_info
+
+        for segment in segments:
+            drawing.segments(
+                framebuffer,
+                colors.GREEN,
+                False,
+                segment,
+                self.__line_width__)
 
         roll = int(roll)
+
+        is_not_visible_x = line_center[0] < 0 or line_center[0] > self.__width__
+        is_not_visible_y = (line_center[1] + self.__font_height__ < 0) \
+            or (line_center[1] - self.__font_height__ > self.__height__)
+
+        if is_not_visible_x or is_not_visible_y:
+            return
 
         self.__render_centered_text__(
             framebuffer,
@@ -93,28 +102,48 @@ class ArtificialHorizon(AhrsElement):
             orientation {orientation} -- The airplane's orientation (roll & pitch)
         """
 
-        # Creating aliases to the functions saves time...
-        pitch = orientation.pitch
-        roll = orientation.roll
-
         # Calculating the coordinates ahead of time...
-        lines_centers_and_angles = [self.__get_line_coords__(
-            pitch, roll, reference_angle) for reference_angle in self.__reference_angles__]
-        # ... only to use filter to throw them out saves time.
-        # This allows for the cores to be used and removes the conditionals
-        # from the actual render function.
-        lines_centers_and_angles = list(filter(
-            lambda center:
-            center[1][1] >= 0 and center[1][1] <= self.__height__, lines_centers_and_angles))
+        segments_centers_and_angles = [self.__get_segment__(
+            orientation.pitch,
+            orientation.roll,
+            reference_angle) for reference_angle in self.__reference_angles__]
 
         # pylint: disable=expression-not-assigned
-        [self.__render_reference_line__(framebuffer, line_info, roll)
-            for line_info in lines_centers_and_angles]
+        [self.__render_horizon_reference__(
+            framebuffer,
+            segments,
+            orientation.roll) for segments in segments_centers_and_angles]
 
-    def __get_line_coords__(
+    def __get_segment_endpoints__(
         self,
-        pitch: float,
-        roll: float,
+        length: int,
+        pitch_offset: float,
+        roll: float
+    ) -> list:
+        roll_delta = 90 - roll
+
+        center_x = self.__center_x__ - \
+            (pitch_offset * fast_math.cos(roll_delta)) + 0.5
+        center_y = self.__center_y__ - \
+            (pitch_offset * fast_math.sin(roll_delta)) + 0.5
+
+        x_len = length * fast_math.cos(roll) + 0.5
+        y_len = length * fast_math.sin(roll) + 0.5
+
+        half_x_len = x_len / 2.0
+        half_y_len = y_len / 2.0
+
+        start_x = int(center_x - half_x_len)
+        end_x = int(center_x + half_x_len)
+        start_y = int(center_y + half_y_len)
+        end_y = int(center_y - half_y_len)
+
+        return [[start_x, start_y], [end_x, end_y]], (center_x, center_y)
+
+    def __get_segment__(
+        self,
+        pitch: int,
+        roll: int,
         reference_angle: int
     ):
         """
@@ -129,36 +158,24 @@ class ArtificialHorizon(AhrsElement):
             [tuple] -- An array[4] of the X/Y line coords.
         """
 
-        length = self.__long_line_width__ if reference_angle == 0 else self.__short_line_width__
-
-        roll_int = int(roll)
-
-        ahrs_center_x, ahrs_center_y = self.__center__
+        length = self.__long_segment_length__ if reference_angle == 0 else self.__short_segment_length__
         pitch_offset = self.__pixels_per_degree_y__ * \
             (-pitch + reference_angle)
 
-        roll_delta = 90 - roll_int
+        outter_endpoints, center = self.__get_segment_endpoints__(
+            length,
+            pitch_offset,
+            roll)
 
-        center_x = ahrs_center_x - \
-            (pitch_offset * fast_math.cos(roll_delta)) + 0.5
-        center_y = ahrs_center_y - \
-            (pitch_offset * fast_math.sin(roll_delta)) + 0.5
+        inner_endpoints, _ = self.__get_segment_endpoints__(
+            self.__inner_blank_area_length__,
+            pitch_offset,
+            roll)
 
-        center_x = int(center_x)
-        center_y = int(center_y)
+        left_segment = [outter_endpoints[0], inner_endpoints[0]]
+        right_segment = [outter_endpoints[1], inner_endpoints[1]]
 
-        x_len = int(length * fast_math.cos(roll_int) + 0.5)
-        y_len = int(length * fast_math.sin(roll_int) + 0.5)
-
-        half_x_len = x_len >> 1
-        half_y_len = y_len >> 1
-
-        start_x = center_x - half_x_len
-        end_x = center_x + half_x_len
-        start_y = center_y + half_y_len
-        end_y = center_y - half_y_len
-
-        return [[start_x, start_y], [end_x, end_y]], (center_x, center_y), reference_angle
+        return [left_segment, right_segment], center, reference_angle
 
 
 if __name__ == '__main__':
