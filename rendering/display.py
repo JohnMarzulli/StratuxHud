f10545445004e9c696aaeeec7e352d326ddd0f68
John Marzulli <john.marzulli@hotmail.com>
2021-03-09 22:54:29 -0800

    Try using RLE to speed up Pi3 perf

--- rendering/display.py
+++ rendering/display.py
@@ -8,12 +8,12 @@ import pygame
 from common_utils import local_debug
 
 # The SunFounder 5" TFT
-DEFAULT_SCREEN_SIZE = 800, 480 #1600, 960
+DEFAULT_SCREEN_SIZE = 800, 480  # 1600, 960
 
 
 def __get_screen_size_and_mode__():
     size = DEFAULT_SCREEN_SIZE
-    screen_mode = pygame.HWACCEL
+    screen_mode = pygame.HWACCEL | pygame.constants.RLEACCEL
 
     if local_debug.IS_PI:
         screen_mode |= pygame.FULLSCREEN
@@ -29,41 +29,40 @@ def display_init():
     Initializes PyGame to run on the current screen.
     """
 
-    size = DEFAULT_SCREEN_SIZE
+    size, screen_mode = __get_screen_size_and_mode__()
     display_number = os.getenv('DISPLAY')
+
     if display_number:
-        screen_mode = (
-            pygame.FULLSCREEN if local_debug.IS_PI else pygame.RESIZABLE) | pygame.HWACCEL
         print("Running under X{}, flags={}".format(display_number, screen_mode))
         screen = pygame.display.set_mode(size, screen_mode)
-    else:
-        if local_debug.IS_MAC:
-            screen = pygame.display.set_mode(size)
 
-            return screen, size
+        return screen, size
 
-        # List of drivers:
-        # https://wiki.libsdl.org/FAQUsingSDL
-        drivers = ['fbcon', 'directfb', 'svgalib', 'directx', 'windib', 'Quartz']
-        found = False
-        for driver in drivers:
-            if not os.getenv('SDL_VIDEODRIVER'):
-                os.putenv('SDL_VIDEODRIVER', driver)
+    if local_debug.IS_MAC:
+        screen = pygame.display.set_mode(size)
 
-            try:
-                pygame.display.init()
-            except pygame.error as ex:
-                print('Driver: {0} failed. EX={1}'.format(driver, ex))
-                continue
+        return screen, size
 
-            found = True
-            break
+    # List of drivers:
+    # https://wiki.libsdl.org/FAQUsingSDL
+    drivers = ['fbcon', 'directfb', 'svgalib', 'directx', 'windib', 'Quartz']
+    found = False
+    for driver in drivers:
+        if not os.getenv('SDL_VIDEODRIVER'):
+            os.putenv('SDL_VIDEODRIVER', driver)
 
-        if not found:
-            raise Exception('No suitable video driver found!')
+        try:
+            pygame.display.init()
+        except pygame.error as ex:
+            print('Driver: {0} failed. EX={1}'.format(driver, ex))
+            continue
 
-        (size, screen_mode) = __get_screen_size_and_mode__()
+        found = True
+        break
 
-        screen = pygame.display.set_mode(size, screen_mode)
+    if not found:
+        raise Exception('No suitable video driver found!')
+
+    screen = pygame.display.set_mode(size, screen_mode)
 
     return screen, size
