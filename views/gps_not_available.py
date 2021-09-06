783fe4ce83515ad162a6acba3681ad90947acf79
John Marzulli <john.marzulli@hotmail.com>
2021-09-06 10:56:30 -0700

    Add GPS INOP error message

---
+++ views/gps_not_available.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+from data_sources.ahrs_data import AhrsData
+from rendering import colors
+
+from views import hud_elements
+from views.ahrs_element import AhrsElement
+
+
+class GpsNotAvailable(AhrsElement):
+    def __init__(
+        self,
+        degrees_of_pitch: float,
+        pixels_per_degree_y: float,
+        font,
+        framebuffer_size,
+        reduced_visuals: bool = False
+    ):
+        super().__init__(font, framebuffer_size, reduced_visuals)
+
+        text_y_pos = self.__bottom_border__ - (self.__font_height__ << 1)
+
+        self.__position__ = [self.__left_border__, text_y_pos]
+
+    def render(
+        self,
+        framebuffer,
+        orientation: AhrsData
+    ):
+        if orientation.gps_online:
+            return
+
+        current_time = datetime.utcnow()
+        is_shown = (current_time.second % 2) != 0
+
+        if not is_shown:
+            return
+
+        self.__render_text__(
+            framebuffer,
+            "ERROR: GPS",
+            self.__position__,
+            colors.RED,
+            0.5)
+
+
+if __name__ == '__main__':
+    hud_elements.run_hud_element(GpsNotAvailable, True)
