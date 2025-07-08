d714e23b0fd17672e8d60870c292ab92433d6116
John Marzulli <john.marzulli@hotmail.com>
2025-07-08 15:56:14 -0700

    Add ability to change video flip from keypad.

--- heads_up_display.py
+++ heads_up_display.py
@@ -694,6 +694,8 @@ class HeadsUpDisplay(object):
         # Quit to terminal only.
         if event.key in [pygame.K_q]:
             return InputResponse(True, False, event)
+        elif event.key == pygame.K_TAB:
+            CONFIGURATION.next_video_flip_mode()
         elif event.key in [pygame.K_KP_PLUS, pygame.K_RIGHT, pygame.K_KP6]:
             CONFIGURATION.next_view(self.__hud_views__)
         elif event.key in [pygame.K_KP_MINUS, pygame.K_LEFT, pygame.K_KP4]:
